87f4c3bf716df067d5acf55b8d89d5a73b91dcdc
zahrakh1998 <zahrakhodabakhshian54@gmail.com>
2023-06-21 13:54:35 +0200

    vehicle-purchase exercise

--- javascript/vehicle-purchase/vehicle-purchase.js
+++ javascript/vehicle-purchase/vehicle-purchase.js
@@ -11,7 +11,7 @@
  * @returns {boolean} whether a license is required
  */
 export function needsLicense(kind) {
-  throw new Error('Please implement the needsLicense function');
+  return (kind === 'car' || kind === 'truck')
 }
 
 /**
@@ -23,7 +23,9 @@ export function needsLicense(kind) {
  * @returns {string} a sentence of advice which option to choose
  */
 export function chooseVehicle(option1, option2) {
-  throw new Error('Please implement the chooseVehicle function');
+    if (option1 < option2)
+      return `${option1} is clearly the better choice.`
+      return `${option2} is clearly the better choice.`
 }
 
 /**
@@ -35,5 +37,11 @@ export function chooseVehicle(option1, option2) {
  * @returns {number} expected resell price in the dealership
  */
 export function calculateResellPrice(originalPrice, age) {
-  throw new Error('Please implement the calculateResellPrice function');
+  if(age < 3)
+    return 0.8 * originalPrice
+  else if( age > 10)
+  return 0.5 * originalPrice
+  else
+  return 0.7 * originalPrice
+ 
 }
